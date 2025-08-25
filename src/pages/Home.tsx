a0a5cddafec1c4b23c39b028666a7c9f34dbfe24
Macarron <macarront001@gmail.com>
2025-08-25 14:57:38 -0500

    global styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,21 @@ import React from 'react'
 
 const Home:React.FC = () => {
   return (
-    <div className='w-full h-full flex justify-center items-center'>
-      Estamos en el hom
-    </div>
-  )
+    <>
+      <div className="w-full h-full flex justify-center items-center">
+        Estamos en el hom
+      </div>
+      <h1 className="font-(family-name:--font-jersey-15) text-9xl text-(--color-title)">
+        Coco
+      </h1>
+      <div className="w-full h-full flex justify-center items-center text-(--color-primary)">
+        Estamos en el hom
+      </div>
+      <h1 className="font-(family-name:--font-jersey-15) text-9xl text-(--color-secondary)">
+        Coco
+      </h1>
+    </>
+  );
 }
 
 export default Home
